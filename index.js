d33f06b6d672107bb1c3d835c688f44f64583a1b
AlexMel83 <meleshko.a.n@ukr.net>
2024-06-25 16:15:41 +0300

    chenge method

--- index.js
+++ index.js
@@ -1,93 +1,64 @@
-const fs = require('fs').promises;
-const path = require('path');
-const process = require('process');
-const {authenticate} = require('@google-cloud/local-auth');
+require('dotenv').config();
+const express = require('express');
 const {google} = require('googleapis');
+const {PORT, CLIENT_ID, CLIENT_SECRET, REDIRECT} = process.env;
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const app = express();
+const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT);
 
-/**
- * Reads previously authorized credentials from the save file.
- *
- * @return {Promise<OAuth2Client|null>}
- */
-async function loadSavedCredentialsIfExist() {
-  try {
-    const content = await fs.readFile(TOKEN_PATH);
-    const credentials = JSON.parse(content);
-    return google.auth.fromJSON(credentials);
-  } catch (err) {
-    return null;
-  }
-}
+app.get('/',(req,res)=>{
+    const url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: 'https://www.googleapis.com/auth/calendar.readonly'
+    });
+    res.redirect(url);
+});
 
-/**
- * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
-async function saveCredentials(client) {
-  const content = await fs.readFile(CREDENTIALS_PATH);
-  const keys = JSON.parse(content);
-  const key = keys.installed || keys.web;
-  const payload = JSON.stringify({
-    type: 'authorized_user',
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token,
-  });
-  await fs.writeFile(TOKEN_PATH, payload);
-}
+app.get('/redirect',(req,res)=>{
+    const code=req.query.code;
+    oauth2Client.getToken(code,(err,tokens)=>{
+        if (err) {
+            console.error('Couldn\'t get token', err);
+            res.send('Error');
+            return;
+        }
+        oauth2Client.setCredentials(tokens);
+        res.send('Succesfully logged in');
+    })
+});
 
-/**
- * Load or request or authorization to call APIs.
- *
- */
-async function authorize() {
-  let client = await loadSavedCredentialsIfExist();
-  if (client) {
-    return client;
-  }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
-  });
-  if (client.credentials) {
-    await saveCredentials(client);
-  }
-  return client;
-}
+app.get('/calendars',(req,res)=>{
+    const calendar = google.calendar({version: 'v3', auth: oauth2Client});
+    calendar.calendarList.list({},(err,response)=>{
+        if(err){
+            console.error('error fetching calendars', err);
+            res.send('Error');
+            return;
+        }
+        const calendars = response.data.items;
+        res.json(calendars);
+    })
+})
 
-/**
- * Lists the next 10 events on the user's primary calendar.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-async function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  const res = await calendar.events.list({
-    calendarId: 'primary',
-    timeMin: new Date().toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  });
-  const events = res.data.items;
-  if (!events || events.length === 0) {
-    console.log('No upcoming events found.');
-    return;
-  }
-  console.log('Upcoming 10 events:');
-  events.map((event, i) => {
-    const start = event.start.dateTime || event.start.date;
-    console.log(`${start} - ${event.summary}`);
-  });
-}
+app.get('/events',(req,res)=>{
+    const calendarId = req.query.calendar??'primary';
+    const calendar = google.calendar({version: 'v3', auth: oauth2Client});
+    calendar.events.list({
+        calendarId,
+        timeMin: (new Date()).toISOString(),
+        maxResults: 15,
+        signleEvents: true,
+        orderBy: 'satrTime',
 
-authorize().then(listEvents).catch(console.error);
+    },(err,response)=>{
+        if(err){
+            console.error('Can\'t fetch events');
+            res.send('Error');
+            return;
+        }
+        const events = response.data.items;
+        res.json(events);
+    })
+})
+
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
